f5194f153b163248b4c0d907688073aed27d93f3
sjaiaiqqqq-creator <sjaiaiqqqq@gmail.com>
2026-06-10 19:48:35 +0900

    Update app.py

--- app.py
+++ app.py
@@ -27,5 +27,5 @@ PAGE = f'''<!DOCTYPE html>
 def home():
     return render_template_string(PAGE)
 
-if name == 'main':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
